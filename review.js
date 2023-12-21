a5ac1c20bb89bdc94c2701e2fdc2ea3ed78be067
josecobi <58313777+josecobi@users.noreply.github.com>
2023-12-21 16:06:05 -0500

    slice

--- review.js
+++ review.js
@@ -92,7 +92,7 @@ const joshIsLookingForWifey = "WIFEY 1, wifey 2, WiFeY 3, Wifey 4"
 console.log(joshIsLookingForWifey.search("wifey"));
 // I guess we can share this pizza sentence. Use slice() to return the other half of pizza.
 const pizzaSentence = "pizza, other half of pizza"
-
+console.log(pizzaSentence.slice(pizzaSentence.length / 2));
 // Now using the pizza sentence, return only pizza (before the comma)
 
 // Okay, but who decided to go to this expensive restaurant? Yaz, Poornima, and Leshawn decided to split() the bill. Return an array separating Yaz, Poornima, and Leshawn.
